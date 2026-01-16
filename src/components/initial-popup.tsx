eb67c51fe858ebfc71b579d3c61e4b7ee1a921da
Younus Shamim <younusshamim.bd@gmail.com>
2026-01-16 12:35:17 +0600

    Add new promotional image and re-enable InitialPopup component in layout; update image source in InitialPopup for consistency.

--- src/components/initial-popup.tsx
+++ src/components/initial-popup.tsx
@@ -62,7 +62,7 @@ const InitialPopup = () => {
       <AlertDialogContent className="w-[93%] sm:w-[90%] md:max-w-[600px] p-0 rounded-lg">
         <div className="relative">
           <Image 
-            src='/images/promotion/promotion1.jpeg' 
+            src='/images/promotion/promotion2.jpg' 
             alt='Promotional Offer' 
             width={600} 
             height={600}
